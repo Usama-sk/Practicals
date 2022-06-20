bf2b73a350b7ed080fe98dbb6e0b6b56ab26f459
Usama-sk <usamask60@gmail.com>
2022-06-20 12:16:07 +0500

    Add project files.

---
+++ Practicals/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Practicals
+{
+    class Program
+    {
+        // Practical No 1 
+        public string fobbonic_series(int input)
+        {
+            return "ss";
+        }
+
+
+        // Practical No 1 
+        public string fobbonic_series(int input)
+        {
+            return "ss";
+        }
+
+
+        static void Main(string[] args)
+        {
+            
+            
+
+
+        }
+    }
+}
